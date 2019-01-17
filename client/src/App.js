887c39b0f88d74250daf6769631312cc8874dccf
William George Cook <wgeorgecook@gmail.com>
2019-01-16 20:11:13 -0800

    Remove all items render

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import NewItem from './NewItem';
 
 import './App.css';
-import AllItems from './AllItems';
+
 
 class App extends Component {
   render() {
     return (
       <div className="root">
         <NewItem />
-        <AllItems />
       </div>
     );
   }
