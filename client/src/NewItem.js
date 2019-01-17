d03d1fbf8ed2ef042c9a9418f4f10e5d5ffa2eac
William George Cook <wgeorgecook@gmail.com>
2019-01-16 19:55:50 -0800

    Submit handler for post requests

--- client/src/NewItem.js
+++ client/src/NewItem.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 
 export default class NewItem extends Component {
 
-  handleChange = () => {
+  state = {
+    name: null,
+    carbs: null,
+    error: null
+  }
 
+  handleChange = (e) => {
+    const key = e.target.className;
+    const value = e.target.value;
+    this.setState({ [key]: value })
+  }
+
+  submitItem = (e) => {
+    e.preventDefault();
+    const { name, carbs } = this.state;
+    if (!name || !carbs) return;
+    fetch('/items', {
+      method: 'POST',
+      headers: { 'Content type': 'application/json' },
+      body: JSON.stringify({ name, carbs })
+    })
+    .then(res => res.json())
+    .then( res => {
+      if(!res.success) this.setState( { error: res.error.message || res.error });
+      else this.setState( { name: null, carbs: null, error: null })
+    })
   }
 
   render() {
     return (
       <div className="newitem">
-        <input type='text' placeholder="Item name" className="newitemname" onChange={ this.handleChange }/>
-        <input type='number' placeholder="Carbohydrates" className="newitemcarbs" />
-        <button type='submit'>Add item</button>
+        <input type='text' placeholder="Item name" className="name" onChange={ this.handleChange }/>
+        <input type='number' placeholder="Carbohydrates" className="carbs" onChange={ this.handleChange }/>
+        <button type='submit' onSubmit={ this.submitItem }>Add item</button>
       </div>
     )
   }
