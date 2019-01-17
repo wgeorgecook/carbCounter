4abcd032db3041d0d56d55258b23851aa8880fc3
William George Cook <wgeorgecook@gmail.com>
2019-01-16 17:51:19 -0800

    Add state

--- client/src/AllItems.js
+++ client/src/AllItems.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class AllItems extends Component {
 
+  state = {
+    error: null,
+    data: []
+  }
+
   componentDidMount() {
     this.loadAllItems();
   }
