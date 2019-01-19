f1abdb656ce16b2b6bdaa26f05dffafd2f5b689d
William George Cook <wgeorgecook@gmail.com>
2019-01-19 07:54:42 -0800

    Remove hardcoded items

--- client/src/HoldItems.js
+++ client/src/HoldItems.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 
 export default class HoldItems extends Component {
 
-  state = {
-    items: [{name: "Wine", carbs: 14}, {name: "Coffee", carbs: 14}]
-  }
-
   render() {
     return (
-      <div className="holditems">
-        Your items:
-        {this.state.items.map(item => (
-          <ol className="item"> {item.name}
-            <li key={item._id}>Carbs: {item.carbs}</li>
-        </ol> )
-        )}
-      </div>
+      (this.props.items)
+      ? <div className="holditems">
+          Your items:
+          {this.props.items.map(item => (
+            <ol className="item"> {item.name}:
+              <li key={item._id}>Carbs: {item.carbs}</li>
+          </ol> )
+          )}
+        </div>
+      : <div></div>
     )
   }
 }
