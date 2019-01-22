a65741f821544bf69ac3b98bc56fff141d4f9195
William George Cook <wgeorgecook@gmail.com>
2019-01-22 14:16:14 -0800

    Method to hide new item field unless needed

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,23 @@ import NewItem from './NewItem';
 
 
 class App extends Component {
+  state = {
+    new: false
+  }
+
+  showNew = () => {
+    this.setState({new: true});
+  }
+
   render() {
     return (
       <div className="root">
+        <div className="top">Welcome to Carb Counter</div>
         <SearchItems />
-        <NewItem />
+      {(this.state.new === true)
+        ?  <NewItem />
+        : <button className="addNew" onClick={this.showNew}>Add new item</button>
+          }
       </div>
     );
   }
