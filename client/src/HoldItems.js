27d3e31efa82d1f64b407bcfc1fd8975e07adaab
William George Cook <wgeorgecook@gmail.com>
2019-01-24 19:50:20 -0800

    Displays total carbs placeholder

--- client/src/HoldItems.js
+++ client/src/HoldItems.js
@@ -3,24 +3,31 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
+
 export default class HoldItems extends Component {
+
 
   render() {
     return (
       (this.props.items)
       ? <div className="heldItems">
           <List>
-            {this.props.items.map((item) => {
+            {this.props.items.map((item, i) => {
               return(
-                <ListItem>
+                <ListItem key={i}>
                   <ListItemText
                     primary={item.label}
                     secondary={item.carbs}
                   />
                 </ListItem>
               )
-            })
-          }
+              })}
+            <ListItem>
+              <ListItemText
+                  primary="Total carbs"
+                  secondary={ this.props.allCarbs }
+                />
+            </ListItem>
           </List>
         </div>
       : null
