24117e329b541d3d2696ef859b4c1c8af31936a0
William George Cook <wgeorgecook@gmail.com>
2019-01-26 16:42:53 -0800

    Servings now update total carbs accordingly

--- client/src/TotalCarbs.js
+++ client/src/TotalCarbs.js
@@ -4,14 +4,24 @@ import ListItemText from '@material-ui/core/ListItemText';
 
 export default class TotalCarbs extends Component {
 
+  state = {
+    total: 0
+  }
+
+  componentWillReceiveProps = () => {
+    const total = this.props.sum.map(item => (item.carbs * item.servings)).reduce( (sum, item) => { return sum + item } )
+    console.log(total)
+    this.setState({ total })
+  }
 
   render() {
+
     return (
       <div className="totalcarbs">
           <ListItem>
             <ListItemText
                 primary="Total carbs"
-                secondary={ 0}
+                secondary={ this.state.total }
               />
           </ListItem>
       </div>
