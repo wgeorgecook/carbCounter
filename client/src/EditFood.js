2d84f18e20922b68ed4e5cb9ad23598906557b8b
William George Cook <wgeorgecook@gmail.com>
2019-02-09 11:00:17 -0800

    Skeleton handler function

--- client/src/EditFood.js
+++ client/src/EditFood.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
 export default class EditFood extends Component {
+
+  handleSubmit = () => {
+    console.log("Button push")
+  }
+
   render() {
     return (
       <div className="editfood">
         <Button
           variant="contained"
           color="primary"
-          type='submit'> Edit
+          type='submit'
+          onClick={this.handleSubmit}> Edit
           </Button>
       </div>
     )
