407a466b52d1bf0009a62f27d8c3218909988b38
William George Cook <wgeorgecook@gmail.com>
2019-02-11 14:32:37 -0800

    Able to close edit menu

--- client/src/EditFood.js
+++ client/src/EditFood.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 export default class EditFood extends Component {
 
+  state = {
+    open: false
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({open: nextProps.open})
+  }
+
   render() {
     return (
       <div className="editfood">
-        <p>Edit menu goes here</p>
+        {(this.state.open)
+        ? <button onClick={this.props.onClose}>x</button>
+        : null
+        }
       </div>
     )
   }
