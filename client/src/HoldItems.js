408edc63f2d51974f04fc9dbe6e477ff772e0ce9
William George Cook <wgeorgecook@gmail.com>
2019-02-24 20:31:33 -0800

    Remove old list imports

--- client/src/HoldItems.js
+++ client/src/HoldItems.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-/*
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { Input, Divider } from '@material-ui/core';
-*/
 import { Input } from '@material-ui/core';
 import EditFood from './EditFood'
 import TotalCarbs from './TotalCarbs';
