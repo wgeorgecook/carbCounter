7c50b947b7002a5b89952f35b0ec4e01495bde78
William George Cook <wgeorgecook@gmail.com>
2019-03-30 09:39:42 -0700

    Template for cognito data

---
+++ client/src/cognitoConfigTemplate.js
@@ -0,0 +1,8 @@
+// Rename me to cognitoClient.js and populate with your data
+
+const ids = {
+    "UserPoolId": "yourUserPoolId",
+    "ClientId": "yourClientId"
+}
+
+export const getId = key => ids[key]
